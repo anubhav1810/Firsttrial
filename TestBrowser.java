0a0dab4ff3ed88f3e64e20f6735dc5bde115388e
anubhav1810 <121294958+anubhav1810@users.noreply.github.com>
2023-01-29 20:19:35 +0530

    hello i am string value

--- TestBrowser.java
+++ TestBrowser.java
@@ -1,5 +1,6 @@
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -22,25 +23,44 @@ public class TestBrowser {
 		DesiredCapabilities ds = new DesiredCapabilities();
 		//DesiredCapabilities ds = new DesiredCapabilities();
 		ds.setCapability("os", "Windows");
-		ds.setCapability("os_version", "10");
-		ds.setCapability("browser", "Chrome");
+		ds.setCapability("os_version", "8");
+		ds.setCapability("browser", "Firefox");
 		ds.setCapability("browser_version", "latest");
 		ds.setCapability("resolution", "1280x800");
 		ds.setCapability("project", "Multibrowser testing");
 		ds.setCapability("build", "Build_01");
+		ds.setCapability("name","honda.com");
 		ds.setCapability("browserstack.local", "false");
 		ds.setCapability("browserstack.selenium_version", "3.14.0");
 		
 		WebDriver driver = new RemoteWebDriver(new URL(url), ds);
-		driver.get("https://google.com");
-		WebElement editbox =driver.findElement(By.name("q"));
+		driver.get("https://www.honda.com/#");
+		List<WebElement> numberofWebElement;
+		numberofWebElement = driver.findElements(By.xpath("//a"));
+		System.out.print(url);
+	     String price = "Rs 100,400";
+	     int lengyth =price.length();
+	     
+	     String separate = price.substring(3, lengyth-1);
+	     String[] neString = separate.split(",");
+	     System.out.println(separate);
+		  int sum = 0; String value;
+	     for(int i=0;i<=neString.length;i++)
+	     {  value=neString[i];
+	         System.out.println("price of product:"+i+"and there value"+ value);
+	    	 sum= sum+Integer.parseInt(value);
+	    	 System.out.print(sum);
+	     }
+	     System.out.println("Sum"+sum);
+	     
+	/*	WebElement editbox =driver.findElement(By.name("q"));
 		editbox.click();
 		editbox.clear();
-		editbox.sendKeys("Infosys");
-	    System.out.println(editbox.getText());
+		editbox.sendKeys("Infosys"); */
+	//    System.out.println(editbox.getText());
 	    
 	    
-		editbox.submit();
+	//	editbox.submit();
 		
 		
 	/*	Actions a = new Actions(driver);
